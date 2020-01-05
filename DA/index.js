29ac4fc116b05fa62d5bd6eeb515c0130c5d2513
Queejon <queejonjin@gmail.com>
2020-01-04 22:30:21 -0600

    npm startup improvements

--- DA/index.js
+++ DA/index.js
@@ -1,1 +1,46 @@
-console.log('App not yet implemented...like, any of it');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+   <App />,
+   document.getElementById('root')
+ );
+
+ class App extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         name: 'Galanarius',
+         ind: [{
+            pages: Array(5).fill(null),
+         }],
+      }
+   }
+ }
+
+ class Page extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         title: 'blank',
+         id: -1,
+      }
+   }
+
+   setHome(p){
+
+   }
+   
+   setAbout(p){
+
+   }
+
+   setProfile(p){
+
+   }
+
+   setOperations(p){
+
+   }
+
+ }
